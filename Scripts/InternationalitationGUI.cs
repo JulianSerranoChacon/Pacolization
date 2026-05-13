72502b6cde3b7ce807ac8e46eb21ff3876dc7058
luisja30 <99479754+luisja30@users.noreply.github.com>
2026-05-13 15:24:08 +0200

    Seleecion de ubicacion para XML
    Se abre una ventana del explorador para que el jugador seleccione la ubicación que le apetezca

--- Scripts/InternationalitationGUI.cs
+++ Scripts/InternationalitationGUI.cs
@@ -8,6 +8,8 @@ public class InternationalitationGUI : EditorWindow
 {
     ExtractClass extract = new ExtractClass();
     FileClass file = new FileClass();
+
+    private string dir;
     private string langNum;
 
     // Incluye una entrada en el menu superior de Unity
@@ -23,6 +25,7 @@ public class InternationalitationGUI : EditorWindow
     void OnGUI()
     {
         GUILayout.Label("Plugin Configuration", EditorStyles.boldLabel);
+        
         EditorGUILayout.Space();
 
         // Boton que ejecuta el script de modificacion de los strings
@@ -36,23 +39,22 @@ public class InternationalitationGUI : EditorWindow
         {
             ExtractStrings();
         }
-        // Boton que busca tus Scriptables
-        if (GUILayout.Button("Get All ScriptableObjects"))
-        {
-            FindAllScriptableObjects();
-        }
 
-        //Boton que ejecuta la escritura las cadenas de strings a un CSV
+        EditorGUILayout.Space();
+
+        //Boton que ejecuta la escritura las cadenas de strings a un XML
         if (GUILayout.Button("Write To XML"))
         {
             WriteToXML();
         }
 
-        //Boton que ejecuta la lectura de las cadenas de strings de un CSV concreto
+        //Boton que ejecuta la lectura de las cadenas de strings de un XML concreto
         if (GUILayout.Button("Read from XML"))
         {
             ReadFromXML();
         }
+
+
     }
 
     void ModifyStrings()
@@ -64,19 +66,35 @@ public class InternationalitationGUI : EditorWindow
     {
         extract.ExtractStrings();
     }
-    void FindAllScriptableObjects()
-    {
-        extract.FindAssetsByType<ScriptableObject>();
-    }
+
     void WriteToXML()
     {
-        string path = Application.streamingAssetsPath + "/xml/ejemplo.xml";
-        file.WriteXML(path);
+        //Abre una ventana del explorador para qe
+        string selectedPath = EditorUtility.SaveFilePanel(
+            "Select directory to save XML",
+            Application.dataPath, 
+            "example.xml", //Nombre por defecto
+            "xml");
+
+        if (!string.IsNullOrEmpty(selectedPath))
+        {
+            file.WriteXML(dir);
+            Debug.Log("File saved in: " + selectedPath);
+        }
     }
 
     void ReadFromXML()
     {
-        string path = Application.streamingAssetsPath + "/xml/ejemplo_Idiomas.xml";
-        file.ReadXML(path);
+        //Abre una ventana en la que el juador a�ada la ruta en la que quiera 
+        string selectedPath = EditorUtility.OpenFilePanel(
+          "Select XML File to read",
+          Application.dataPath,
+          "xml");
+
+        if (!string.IsNullOrEmpty(selectedPath))
+        {
+            file.ReadXML(selectedPath);
+            Debug.Log("File load from: " + selectedPath);
+        }
     }
 }
