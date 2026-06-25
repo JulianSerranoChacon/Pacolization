55fd22377504fc87e7dc11041f69997d8a4a2842
VerXxuxZ <juserr04@ucm.es>
2026-06-25 18:37:01 +0200

    he borrado las llamadas de debug
    Co-Authored-By: Javalonso1 <123580168+Javalonso1@users.noreply.github.com>
    Co-Authored-By: luisja30 <99479754+luisja30@users.noreply.github.com>

--- Scripts/Pacolization.cs
+++ Scripts/Pacolization.cs
@@ -34,10 +34,6 @@ public class Pacolization : MonoBehaviour
             Li = LocalInterface.Instance();
             Li.Initiate(scanScriptables, scriptablePath);
             Li.StartInExecution(filePath[currentLang], currentLang, confPath[currentLang]);
-           
-            WriteVariables("item", "camisa");
-            WriteGenderConf("camisa", 1);
-            WriteVariables("objeto", "[\"!{item}\": El|La] !{item}");
 
         }
         else
@@ -50,12 +46,6 @@ public class Pacolization : MonoBehaviour
     {
         currentLang =(uint) id;
         Li.changeLang(filePath[id], (uint)id, confPath[currentLang]);
-    }
-
-    public void test()
-    {
-        WriteVariables("item", "boligrafo");
-        WriteGenderConf("boligrafo", 0);
     }
 
     public void WriteGenderConf(string key, int value)
